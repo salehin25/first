70294bfa1337dba9668ee8c60a7918ae84fec9e2
Anjum <Salehin.Anjum@shell.com>
2020-06-16 20:56:48 +0530

    First Commit

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val;
+            Console.Write("Enter Integer: ");
+            val = Console.ReadLine();
+            Console.WriteLine($"Hello {val}!");
+        }
+    }
+}
